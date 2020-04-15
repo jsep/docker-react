9c59cca3204044bd353d9eb482b6709a5b31cabc
Juan Sepulveda <juan@sepulveda.do>
2020-04-15 13:13:57 -0400

    Fix tests

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is a new change
+          Learn React. Test changed
         </a>
       </header>
     </div>
