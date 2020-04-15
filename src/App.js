a0646539c545e3d3fd8f0a856059309e45e6fad3
Juan Sepulveda <juan@sepulveda.do>
2020-04-15 13:10:49 -0400

    Change app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is a new change
         </a>
       </header>
     </div>
